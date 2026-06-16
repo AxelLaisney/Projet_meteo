9a4539b4e22447661af351f91254762561448109
Enidikay <abdallah.yakidine@gmail.com>
2026-06-16 10:41:20 +0200

    création de update

--- src/repository/releves.repository.js
+++ src/repository/releves.repository.js
@@ -1,10 +1,11 @@
 import { readCSV } from "../utils/csv.js";
 import { config } from "../config.js";
+import { releves_model } from "../../../ProjetAPIMeteo/src/models/releve.model.js";
 
 export class ReleveRepository {
     constructor(cheminCsv) {
         this.cheminCsv = cheminCsv; // l'état encapsulé (plus tard : un cache)
-        this.releve = this.findAll()
+        this.releve = this.findAll().slice(1).map(ligne, index => { releves_model.depuisLigneCsv(ligne, index) })
     }
 
     async findAll() {
@@ -20,12 +21,18 @@ export class ReleveRepository {
         // si ya déjà un id on update sinon on créer
     }
 
-    update(id, data){
+    #update(id, data) {
         const getItem = this.findById(id)
-        if(!getItem){
-            return false
+        if (getItem) {
+            for ({id,ville, date, tempMin, tempMax, desc, hum } in data) {
+                getItem.ville = ville
+                getItem.date = date
+                getItem.tempMin = tempMin
+                getItem.tempMax = tempMax
+                getItem.desc = desc
+                getItem.hum = hum
+            }
         }
-
     }
 
     // update(id, données)
