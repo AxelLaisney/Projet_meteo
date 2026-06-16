ab7c192975380e092f72d529f07c9f6d29010fd4
Enidikay <abdallah.yakidine@gmail.com>
2026-06-16 09:54:51 +0200

    création de findbyid et deletebyid

--- src/repository/releves.repository.js
+++ src/repository/releves.repository.js
@@ -1,26 +1,31 @@
-import { parseCsv } from "../utils/csv.js";
+import { readCSV } from "../utils/csv.js";
 import { config } from "../config.js";
 
 export class ReleveRepository {
     constructor(cheminCsv) {
         this.cheminCsv = cheminCsv; // l'état encapsulé (plus tard : un cache)
+        this.releve = this.findAll()
     }
 
     async findAll() {
-        return parseCsv(this.cheminCsv);
+        return await readCSV(this.cheminCsv);
     }
     // à compléter : findById(id), save(releve) -> attribue releve.id = max(ids) + 1, deleteById(id)
 
-    findById(id){
+    findById(id) {
+        return this.releve.findIndex(r => r.id === id);
+    }
+
+    save(releve) {
 
     }
 
-    save(releve){
+    deleteById(id) {
+        let index = this.releve.findIndex(r => r.id === id);
 
-    }
-
-    deleteById(id){
-
+        if (index !== -1) {
+            releves.splice(index, 1);
+        }
     }
 }
 
